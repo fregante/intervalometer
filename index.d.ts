ebe5aaa263a4e7120ca2c107f7d5be4e25268406
Nick Taylor <nick@iamdeveloper.com>
2019-04-22 16:15:51 -0400

    Added TypeScript types.

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+
+/// <reference lib="dom" />
+
+export interface CancelCallback {
+	(requestId: number): void;
+}
+
+interface IntervalometerReturnValue {
+	start: VoidFunction;
+	stop: VoidFunction;
+}
+
+/**
+ *
+ * @param callback A callback that will be called.
+ * @param request A window's requestAnimationFrame or setTimeout.
+ * @param cancel A callback that will cancel the given intervalometer. Typically a window's cancelAnimationFrame or clearTimeout.
+ * @param millis Time in milliseconds.
+ */
+export function intervalometer(callback: VoidFunction, request: typeof requestAnimationFrame | typeof setTimeout, cancel: CancelCallback, millis?: number): IntervalometerReturnValue;
+
+/**
+ * A time-based intervalometer.
+ *
+ * @param callback A callback that will be invoked after the given timeframe.
+ * @param timeInMillis Time in milliseconds.
+ */
+export function timerIntervalometer(callback: VoidFunction, timeInMillis: number): IntervalometerReturnValue;
+
+/**
+ * A frame-based intervalometer. It uses requestAnimationFrame and calls the provided callback at every frame.
+ * Ideal for animations.
+ *
+ * @param callback A callback that will be called on each animation frame.
+ */
+export function frameIntervalometer(callback: VoidFunction): IntervalometerReturnValue;
---
+++ index.test-d.ts
@@ -0,0 +1,6 @@
+import { expectType } from 'tsd';
+import {intervalometer, frameIntervalometer, timerIntervalometer, IntervalometerReturnValue } from '.'
+
+expectType<IntervalometerReturnValue>(intervalometer(() => { }, setTimeout, () => { }, 1));
+expectType<IntervalometerReturnValue>(frameIntervalometer(() => { }));
+expectType<IntervalometerReturnValue>(timerIntervalometer(() => { }, 1000));
